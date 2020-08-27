d0556e8e78c82d9d3cae750e7c2fc47a75413054
bendall72 <63432837+bendall72@users.noreply.github.com>
2020-08-27 00:02:16 -0400

    new

--- node/node/db.js
+++ node/node/db.js
@@ -2,25 +2,25 @@ const mysql = require('mysql');
 
 const connect_functions = {
     BD01: mysql.createConnection({
-            host: "134.209.41.5",
+            host: "167.172.234.222",
             user: "BD01",
             password:"noKfD9gaaQjRCKhV2ufg",
             database:"BusDev01"
 }),
     BD02: mysql.createConnection({
-            host: "134.209.41.5",
+            host: "167.172.234.222",
             user: "BD02",
             password:"OVXFcHwPdzErYqQGm4uQ",
             database:"BusDev02"
 }),
     Open1: mysql.createConnection({
-        host: "134.209.41.5",
+        host: "167.172.234.222",
         user: "Open1",
         password:"3B84flSIj12uwjvuWuGZ",
         database:"OpenEnroll1000"
 }),
     Ledger: mysql.createConnection({
-    host: "134.209.41.5",
+    host: "167.172.234.222",
     user: "Ledger",
     password:"fkrKNQ1b0ChWUiyfbcRl",
     database:"LEDGER"
